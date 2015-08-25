6ad695da916a2a5e70971c7f5edad9343f0e4f58
Leonardo Merlin <leonardo.merlin@gmail.com>
2015-08-25 09:24:44 -0300

    Wrap with IIFE

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,27 +1,32 @@
-'use strict';
+/* globals require, exports*/
+(function(){
+  'use strict';
 
-var paths = require('./.yo-rc.json')['generator-gulp-angular'].props.paths;
+  var paths = require('./.yo-rc.json')['generator-gulp-angular'].props.paths;
 
-// An example configuration file.
-exports.config = {
-  // The address of a running selenium server.
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
-  //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
+  // An example configuration file.
+  exports.config = {
+    // The address of a running selenium server.
+    //seleniumAddress: 'http://localhost:4444/wd/hub',
+    //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  
-  baseUrl: 'http://localhost:3000',
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+      'browserName': 'firefox'
+    },
 
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
-  specs: [paths.e2e + '/**/*.js'],
+    // chromeOnly: true,
 
-  // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
-};
+    baseUrl: 'http://localhost:3000',
+
+    // Spec patterns are relative to the current working directly when
+    // protractor is called.
+    specs: [paths.e2e + '/**/*.js'],
+
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+      showColors: true,
+      defaultTimeoutInterval: 30000
+    }
+  };
+})();

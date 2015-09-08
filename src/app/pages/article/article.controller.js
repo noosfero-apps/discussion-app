5b4c9b485f44d73c90b0b777f184367ca68c836e
Leonardo Merlin <leonardo.merlin@serpro.gov.br>
2015-09-08 10:04:29 -0300

    Do not limit width of article content

--- src/app/pages/article/article.controller.js
+++ src/app/pages/article/article.controller.js
@@ -7,15 +7,17 @@
 
   /** @ngInject */
   function ArticlePageController(DialogaService, $state, $sce, $log) {
-    $log.debug('ArticlePageController');
-
     var vm = this;
+
     vm.DialogaService = DialogaService;
     vm.$state = $state;
     vm.$sce = $sce;
     vm.$log = $log;
 
     vm.init();
+    vm.loadData();
+
+    vm.$log.debug('ArticlePageController');
   }
 
   ArticlePageController.prototype.init = function() {
@@ -24,6 +26,13 @@
     vm.page = vm.$state.current.name;
     vm.article = null;
     vm.loading = true;
+
+    vm.$log.debug('vm.page', vm.page);
+  };
+
+  ArticlePageController.prototype.loadData = function() {
+    var vm = this;
+
     switch (vm.page){
       case 'sobre':
         vm.DialogaService.getAbout(handleSuccess, handleError);
@@ -43,8 +52,10 @@
     }
 
     function handleError (error) {
+      vm.$log.debug('handleSuccess.error', error);
       vm.loading = false;
       vm.error = error;
     }
   };
+
 })();

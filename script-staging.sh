49ed232968d8248d80dc337ef59bc6f77346a306
Rafael Diego <rafael.gomes@serpro.gov.br>
2015-09-23 13:39:36 -0300

    css do bloco mais medicos

--- script-staging.sh
+++ script-staging.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 gulp clean && gulp build --staging
 git add --all -f dist/
 git commit -m "Update dist"
-git push upstream staging && wget -qO- --no-check-certificate 'ci.serpro/view/Noosfero/job/DialogaApp-Deploy-Homologa%C3%A7%C3%A3o/build?token=DIALOGA_TOKEN' &> /dev/null
+git push origin staging && wget -qO- --no-check-certificate 'ci.serpro/view/Noosfero/job/DialogaApp-Deploy-Homologa%C3%A7%C3%A3o/build?token=DIALOGA_TOKEN' &> /dev/null

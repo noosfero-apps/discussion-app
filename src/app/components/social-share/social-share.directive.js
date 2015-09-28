b762a318730dd9f80994f107d57cdf64d76ae256
Leonardo Merlin <leonardo.merlin@serpro.gov.br>
2015-09-28 15:47:43 -0300

    Fix social-share. Add missing params (image, url, title, etc.)

--- src/app/components/social-share/social-share.directive.js
+++ src/app/components/social-share/social-share.directive.js
@@ -11,10 +11,11 @@
       restrict: 'E',
       templateUrl: 'app/components/social-share/social-share.html',
       scope: {
-        url: '=',
-        image: '=',
-        text: '=',
-        arrowClass: '@',
+        socialVia: '=',
+        socialUrl: '=',
+        socialImage: '=',
+        socialText: '=',
+        arrowClass: '@'
       },
       controller: SocialShareController,
       controllerAs: 'vm',
@@ -27,7 +28,13 @@
     function SocialShareController($log) {
       $log.debug('SocialShareController');
 
-      // var vm = this;
+      var vm = this;
+
+      vm.socialVia = vm.socialVia || "687948707977695"; // 476168325877872
+      vm.socialUrl = vm.socialUrl || "http://dialoga.gov.br/";
+      vm.socialImage = vm.socialImage || "http://dialoga.gov.br/images/logo.png";
+      vm.socialText = vm.socialText || "Conheça o Dialoga Brasil. Dialoga Brasil | O País fica melhor quando VOCÊ PARTICIPA.";
+
     }
   }
 
